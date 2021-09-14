44bddcd0d6e79001f404b58898a00f9c3464616e
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2021-09-14 19:56:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a = "Nikhil";
+var b = "Singh";
+console.log(a);
+console.log(b);
+var c = a.concat(b);
+console.log(c);
+var d = "go away normal guy this won't make any sense to you";
+var e = d.replace("go away normal guy this won't make any sense to ","fell for it, did");
+console.log(e);
